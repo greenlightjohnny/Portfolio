6c55af74dec53c39f731cdccea35772587cff0e4
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-09-22 19:20:42 -0400

    fade in section headings on scroll

--- JS/javascript.js
+++ JS/javascript.js
@@ -13,3 +13,60 @@ function changeP(e) {
 
 
 background.addEventListener('mousemove', changeP)
+
+
+/////// Fade in on scroll //////////
+
+
+let elements = document.querySelectorAll('.hidden');
+
+
+
+
+function checkHeight() {
+    
+    let testt = document.querySelector('.fix');
+    console.log(testt);
+    console.log(window.scrollY + window.innerHeight);
+    console.log(testt.offsetHeight);
+    console.log(testt.offsetTop);
+
+    elements.forEach(i => {
+        const slideInAt = (window.scrollY + window.innerHeight) - i.offsetHeight;
+       
+        const imageBottom = i.offsetTop + i.offsetHeight;
+        
+        const isHalfShow = slideInAt > i.offsetTop;
+       
+        const isNotPast = window.scrollY < imageBottom;
+        
+
+        if(isHalfShow && isNotPast) {
+            i.classList.add('fade-in-element');
+            i.classList.remove('hidden');
+        } else {
+            i.classList.remove('fade-in-element');
+            i.classList.add('hidden');
+        }
+
+    })
+    // let windowHeight = window.innerHeight;
+    // for(let i = 0; i < elements.length; i++) {
+    //     let element = elements[i];
+    //     console.log(element);
+    //     let distanceFromTop = elements[i].getBoundingClientRect().top;
+    //     let distanceFromBottom = elements[i].getBoundingClientRect().bottom;
+
+    //     if(distanceFromTop - windowHeight <= 0) {
+    //         element.classList.add('fade-in-element');
+    //         element.classList.remove('hidden');
+    //     } else if(distanceFromBottom - windowHeight > 20 ) {
+    //         element.classList.remove('fade-in-element');
+    //         element.classList.add('hidden');
+    //     }
+    // }
+}
+
+
+window.addEventListener('scroll', checkHeight);
+window.addEventListener('resize', checkHeight);
